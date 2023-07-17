acc7b330df4c0cf4b32d39598bd0b53e1bd0e59c
jewon119 <jewon119@gmail.com>
2023-07-17 16:56:42 +0900

    Add: create table using raw method

---
+++ main.py
@@ -0,0 +1,48 @@
+import databases
+import sqlalchemy
+from decouple import config
+
+from fastapi import FastAPI, Request
+
+DB_URL = config("DB_URL_TEST")
+
+database = databases.Database(DB_URL)
+metadata = sqlalchemy.MetaData()
+
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String),
+    sqlalchemy.Column("author", sqlalchemy.String),
+)
+
+
+engine = sqlalchemy.create_engine(DB_URL)
+metadata.create_all(engine)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/books/")
+async def get_all_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/books/")
+async def create_book(request: Request):
+    data = await request.json()
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
