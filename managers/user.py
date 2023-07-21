f2189ffb125d9412985ac9ffeee12be500b1b7c7
jewon119 <jewon119@gmail.com>
2023-07-21 15:55:43 +0900

    Add: admin 전용 회원 목록 리스트api 추가

--- managers/user.py
+++ managers/user.py
@@ -30,3 +30,11 @@ class UserManager:
         elif not pwd_context.verify(user_data["password"], user_obj["password"]):
             raise HTTPException(400, "Wrong email or password")
         return AuthManager.encode_token(user_obj)
+
+    @staticmethod
+    async def get_all_users():
+        return await database.fetch_all(user.select())
+
+    @staticmethod
+    async def get_user_by_email(email):
+        return await database.fetch_all(user.select().where(user.c.email == email))
