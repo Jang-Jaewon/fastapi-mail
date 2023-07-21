fe840cafde9fd7222c81e7fe033e15cb5baff723
jewon119 <jewon119@gmail.com>
2023-07-21 16:23:34 +0900

    회원 권한 변경 api

--- resources/user.py
+++ resources/user.py
@@ -19,3 +19,21 @@ async def get_users(email: Optional[str] = None):
     if email:
         return await UserManager.get_user_by_email(email)
     return await UserManager.get_all_users()
+
+
+@router.put(
+    "/users/{user_id}/make-admin",
+    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
+    status_code=204,
+)
+async def make_admin(user_id: int):
+    await UserManager.change_role(RoleType.admin, user_id)
+
+
+@router.put(
+    "/users/{user_id}/make-approver",
+    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
+    status_code=204,
+)
+async def make_approver(user_id: int):
+    await UserManager.change_role(RoleType.approver, user_id)
